3ab71c7b52ed0bf0cdfb48057ddb8920aa73305c
Jonathan <jpierre@suid.fr>
2018-11-07 12:03:34 +0000

    Navbar Not Display by default in page anymore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { FirebaseAuthenticationUiModule, SigninComponent, SignupComponent } from 'firebase-authentication-ui';
+import { FirebaseAuthenticationUiModule, SigninComponent, SignupComponent, AuthGuardService } from 'firebase-authentication-ui';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { EventComponent } from './event/event.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
     NavbarComponent,
     EventComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,7 @@ import { ProfileComponent } from './profile/profile.component';
     FirebaseAuthenticationUiModule.forRoot(environment.fireauthui),
     RouterModule.forRoot(
       [
-        {path: "", component: AppComponent},
+        {path: "", component: HomeComponent, canActivate:[AuthGuardService]},
         {path: "signin", component: SigninComponent},
         {path: "signup", component: SignupComponent}
       ]
