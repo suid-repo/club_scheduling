e20593a68765857d01bc02d818291f0d38a55ee6
Hugo Pilon <S00184457@mail.itsligo.ie>
2018-11-19 13:28:45 +0000

    Added event manager and events list interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RouterModule } from '@angular/router';
 import { EventComponent } from './event/event.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
+import { EventManagerComponent } from './event-manager/event-manager.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { HomeComponent } from './home/home.component';
     NavbarComponent,
     EventComponent,
     HomeComponent,
+    EventManagerComponent,
   ],
   imports: [
     BrowserModule,
