a530844c87068949960b6f0b7816ac816dc722ee
Jonathan <jpierre@suid.fr>
2018-12-15 22:45:13 +0000

    Seed Default HeadCoach User
    TODO : Change the way to detect 1st
     admin

--- WebApplication/Migrations/Configuration.cs
+++ WebApplication/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 namespace WebApplication.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using WebApplication.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WebApplication.Models.ApplicationDbContext>
     {
@@ -18,6 +21,55 @@ namespace WebApplication.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            PasswordHasher ps = new PasswordHasher();
+
+            //MANAGER
+            var manager =
+                new UserManager<ApplicationUser>(
+                    new UserStore<ApplicationUser>(context));
+
+            var roleManager =
+                new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(context));
+
+            //SEED ROLES
+            if (!roleManager.RoleExists("Head Coach"))
+            {
+                roleManager.Create(
+                    new IdentityRole
+                    {
+                        Name = "Head Coach"
+                    });
+            }
+
+            if (!roleManager.RoleExists("Coach"))
+            {
+                roleManager.Create(
+                    new IdentityRole
+                    {
+                        Name = "Coach"
+                    });
+            }
+
+            //SEED DEFAULT ADMIN USER IF NO ONE EXIST
+            if (manager.FindByName("admin@localhost.com") == null)
+            {
+                ApplicationUser firstHeadCoach = new ApplicationUser
+                {
+                    UserName = "admin@localhost.com",
+                    Email = "admin@localhost.com",
+                    PasswordHash = ps.HashPassword("Admin$1"),
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                if (manager.Create(firstHeadCoach).Succeeded)
+                {
+                    manager.AddToRoles(firstHeadCoach.Id, new string[] { "Head Coach", "Coach" });
+                }
+            }
+                
+            
         }
     }
 }
