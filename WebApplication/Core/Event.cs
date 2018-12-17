9eadfad6f99ca2174cd15d1fbc28ee1e257e72c6
Jonathan <jpierre@suid.fr>
2018-12-17 16:17:06 +0000

    Add Datamodel

---
+++ WebApplication/Core/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Models;
+
+namespace WebApplication.Core
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public virtual IEnumerable<Level> Levels { get; set; }
+        public virtual IEnumerable<ApplicationUser> RegisterUsers { set; get; }
+    }
+}
---
+++ WebApplication/Core/Level.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Core
+{
+    public class Level
+    {
+        public int Id { set; get; }
+        public string Name { get; set; }
+    }
+}
