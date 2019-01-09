36f83bfedd7f8653e450bc14d62b93e45efc8c60
Jonathan <jpierre@suid.fr>
2019-01-09 18:36:57 +0100

    DatePicker Added

--- WebApplication/App_Start/BundleConfig.cs
+++ WebApplication/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace WebApplication
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,6 +25,7 @@ namespace WebApplication
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/jquery-ui.css",
                       "~/Content/site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap-rtl").Include(
