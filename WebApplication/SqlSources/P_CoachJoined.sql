9adf9da5f715e5ddd714f473233568d39a799038
Jonathan <jpierre@suid.fr>
2019-01-11 19:56:56 +0100

    Leave Queued System now handle family

--- WebApplication/SqlSources/P_CoachJoined.sql
+++ WebApplication/SqlSources/P_CoachJoined.sql
@@ -2,9 +2,9 @@
 	@eventId int
 AS
 	/**
-	 * THIS STORED PROCEDURE IS CALLED WHEN A COACH LEAVE AN EVENT
+	 * THIS STORED PROCEDURE IS CALLED WHEN A COACH JOIN AN EVENT
 	 * IT WILL RECALCULATE HOW MANY MAX PEOPLE ALLOWED FOR THE EVENT
-	 * THEN MOVE POEPLE
+	 * THEN MOVE PEOPLE
 	 */
 	 DECLARE @maxPeople INT = 0
 	 DECLARE @numPeople INT = 0 --numPeople = people register in the event then people to add to the event
@@ -22,77 +22,78 @@ AS
 	SELECT @maxPeople = COUNT(UserId)*8 - @numPeople
 	FROM CoachEvents
 	WHERE EventId = @eventId
-
-	-- Select the list of family in this event
-	-- We choose MAX function to penalize late members family (join the event seperate with other) 
-	INSERT INTO @familyList
-	SELECT ANU.Family_Id, NULL, MAX(QI.[Time]), COUNT(0)
-	FROM QueuedItems AS QI
-	INNER JOIN AspNetUsers AS ANU ON ANU.Id = QI.UserId
-	WHERE QueuedId = @eventId
-	AND ANU.Family_Id IS NOT NULL
-	GROUP BY ANU.Family_Id
-	HAVING COUNT(0) <= @maxPeople
-	
-	-- Select People W/OUT family
-	INSERT INTO @familyList
-	SELECT 0, QI.UserId, QI.[Time], 1
-	FROM QueuedItems AS QI
-	INNER JOIN AspNetUsers AS ANU ON ANU.Id = QI.UserId
-	WHERE QueuedId = @eventId
-	AND ANU.Family_Id IS NULL
-
-	--SELECT OUR FIRST FAMILY
-	SELECT TOP(1) @selectedFamily = FamilyId, @selectedUser = UserId
-	FROM @familyList
-	WHERE MembersCount <= @maxPeople
-	ORDER BY [Time] ASC, MembersCount DESC
-
-
-	--START WHILE --> LET'S DECREASE THAT QUEUED
-	WHILE(@selectedFamily IS NOT NULL AND @maxPeople > 0)
+	IF(@maxPeople > 0)
 	BEGIN
-		IF(@selectedFamily = 0) -- People W/OUT Family is selected
-			BEGIN
-				INSERT INTO @people2Add VALUES (@selectedUser)
-				
-				SELECT @maxPeople -= 1 -- Decrease maxPeople
-				
-				DELETE FROM @familyList WHERE UserId = @selectedUser -- Clean
-			END
-		ELSE -- Family is selected
-			BEGIN
-				INSERT INTO @people2Add
-				SELECT TOP(@maxPeople - @numPeople) UserId
-				FROM QueuedItems AS QI
-				INNER JOIN AspNetUsers AS ANU ON ANU.Id = QI.UserId
-				WHERE QueuedId = @eventId
-				AND ANU.Family_Id = @selectedFamily
-
-				SELECT @maxPeople -= MembersCount FROM @familyList WHERE FamilyId = @selectedFamily -- Decrease maxPeople
-				
-				DELETE FROM @familyList WHERE FamilyId = @selectedFamily -- Clean
-			END
-
-			--Clean Variables
-			SELECT @selectedFamily = NULL, @selectedUser = NULL
-
-			--Select again a new item
-			SELECT TOP(1) @selectedFamily = FamilyId, @selectedUser = UserId
-			FROM @familyList
-			WHERE MembersCount <= @maxPeople
-			ORDER BY [Time] ASC, MembersCount DESC
-	END
+		-- Select the list of family in this event
+		-- We choose MAX function to penalize late members family (join the event seperate with other) 
+		INSERT INTO @familyList
+		SELECT ANU.Family_Id, NULL, MAX(QI.[Time]), COUNT(0)
+		FROM QueuedItems AS QI
+		INNER JOIN AspNetUsers AS ANU ON ANU.Id = QI.UserId
+		WHERE QueuedId = @eventId
+		AND ANU.Family_Id IS NOT NULL
+		GROUP BY ANU.Family_Id
+		HAVING COUNT(0) <= @maxPeople
 	
-	SELECT @numPeople = COUNT(0)
-	FROM @people2Add
+		-- Select People W/OUT family
+		INSERT INTO @familyList
+		SELECT 0, QI.UserId, QI.[Time], 1
+		FROM QueuedItems AS QI
+		INNER JOIN AspNetUsers AS ANU ON ANU.Id = QI.UserId
+		WHERE QueuedId = @eventId
+		AND ANU.Family_Id IS NULL
 
-	IF (@numPeople > 0)
-	BEGIN
-		EXEC P_Move2Event
-		@eventId = @eventId,
-		@userList = @people2Add
-	END
+		--SELECT OUR FIRST FAMILY
+		SELECT TOP(1) @selectedFamily = FamilyId, @selectedUser = UserId
+		FROM @familyList
+		WHERE MembersCount <= @maxPeople
+		ORDER BY [Time] ASC, MembersCount DESC
+
+
+		--START WHILE --> LET'S DECREASE THAT QUEUED
+		WHILE(@selectedFamily IS NOT NULL AND @maxPeople > 0)
+		BEGIN
+			IF(@selectedFamily = 0) -- People W/OUT Family is selected
+				BEGIN
+					INSERT INTO @people2Add VALUES (@selectedUser)
+				
+					SELECT @maxPeople -= 1 -- Decrease maxPeople
+				
+					DELETE FROM @familyList WHERE UserId = @selectedUser -- Clean
+				END
+			ELSE -- Family is selected
+				BEGIN
+					INSERT INTO @people2Add
+					SELECT TOP(@maxPeople - @numPeople) UserId
+					FROM QueuedItems AS QI
+					INNER JOIN AspNetUsers AS ANU ON ANU.Id = QI.UserId
+					WHERE QueuedId = @eventId
+					AND ANU.Family_Id = @selectedFamily
+
+					SELECT @maxPeople -= MembersCount FROM @familyList WHERE FamilyId = @selectedFamily -- Decrease maxPeople
+				
+					DELETE FROM @familyList WHERE FamilyId = @selectedFamily -- Clean
+				END
+
+				--Clean Variables
+				SELECT @selectedFamily = NULL, @selectedUser = NULL
+
+				--Select again a new item
+				SELECT TOP(1) @selectedFamily = FamilyId, @selectedUser = UserId
+				FROM @familyList
+				WHERE MembersCount <= @maxPeople
+				ORDER BY [Time] ASC, MembersCount DESC
+		END
 	
+		SELECT @numPeople = COUNT(0)
+		FROM @people2Add
+
+		IF (@numPeople > 0)
+		BEGIN
+			EXEC P_Move2Event
+			@eventId = @eventId,
+			@userList = @people2Add
+		END
+	END
 RETURN 0
 
--- WebApplication/SqlSources/P_CoachLeaved.sql
+++ WebApplication/SqlSources/P_CoachLeaved.sql
@@ -7,32 +7,96 @@ AS
 	 * THEN MOVE POEPLE
 	 */
 	 DECLARE @maxPeople INT = 0
-	 DECLARE @numPeople INT = 0
+	 DECLARE @numPeople INT = 0 --numPeople = people register in the event then people to add to the event
+	 DECLARE @selectedFamily INT = NULL
+	 DECLARE @selectedUser NVARCHAR(128) = NULL
 	 DECLARE @insertedTime BIGINT = 0
 	 DECLARE @people2Kick UserListTableType
-	 
-				-- Determine the number of maximum people for this event
-				SELECT @maxPeople = COUNT(0)*8
-				FROM CoachEvents
-				WHERE EventId = @eventId
+	 DECLARE @familyList FamilyQueuedListTableType
 
-				SELECT @numPeople = COUNT(0)
-				FROM ApplicationUserEvents
-				WHERE Event_Id = @eventId
+	-- Get the number of people register to this event
+	SELECT @numPeople = COUNT(ApplicationUser_Id)
+	FROM ApplicationUserEvents
+	WHERE Event_Id = @eventId
 
-				-- SELECT THE NUMBER OF PEOPLE TO KICK
-				IF ((@numPeople - @maxPeople) > 0)
+	-- Determine the number of people to kick
+	SELECT @maxPeople =  @numPeople - COUNT(UserId)*8
+	FROM CoachEvents
+	WHERE EventId = @eventId
+
+	IF(@maxPeople) > 0
+	BEGIN
+		-- Select the list of family registered in this event
+		-- We choose MAX function to penalize late members family (join the event seperate with other) 
+		INSERT INTO @familyList
+		SELECT ANU.Family_Id, NULL, 0, COUNT(0)
+		FROM ApplicationUserEvents AS AUE
+		INNER JOIN AspNetUsers AS ANU ON ANU.Id = AUE.ApplicationUser_Id
+		WHERE Event_Id = @eventId
+		AND ANU.Family_Id IS NOT NULL
+		GROUP BY ANU.Family_Id
+		HAVING COUNT(0) <= @maxPeople
+	
+		-- Select People W/OUT family
+		INSERT INTO @familyList
+		SELECT 0, AUE.ApplicationUser_Id, 0, 1
+		FROM ApplicationUserEvents AS AUE
+		INNER JOIN AspNetUsers AS ANU ON ANU.Id = AUE.ApplicationUser_Id
+		WHERE Event_Id = @eventId
+		AND ANU.Family_Id IS NULL
+
+		--SELECT OUR FIRST FAMILY
+		SELECT TOP(1) @selectedFamily = FamilyId, @selectedUser = UserId
+		FROM @familyList
+		WHERE MembersCount <= @maxPeople
+		ORDER BY newid()
+
+
+		--START WHILE --> LET'S DECREASE THAT QUEUED
+		WHILE(@selectedFamily IS NOT NULL AND @maxPeople > 0)
+		BEGIN
+			IF(@selectedFamily = 0) -- People W/OUT Family is selected
+				BEGIN
+					INSERT INTO @people2Kick VALUES (@selectedUser)
+				
+					SELECT @maxPeople -= 1 -- Decrease maxPeople
+				
+					DELETE FROM @familyList WHERE UserId = @selectedUser -- Clean
+				END
+			ELSE -- Family is selected
 				BEGIN
 					INSERT INTO @people2Kick
-					SELECT TOP(@numPeople - @maxPeople) ApplicationUser_Id
-					FROM ApplicationUserEvents
+					SELECT ApplicationUser_Id
+					FROM ApplicationUserEvents AS AUE
+					INNER JOIN AspNetUsers AS ANU ON ANU.Id = AUE.ApplicationUser_Id
 					WHERE Event_Id = @eventId
-					ORDER BY newid()
-			
-					EXEC P_Move2Queued 
-					@eventId = @eventId,
-					@insertedTime = @insertedTime,
-					@userList = @people2kick
+					AND ANU.Family_Id = @selectedFamily
+
+					SELECT @maxPeople -= MembersCount FROM @familyList WHERE FamilyId = @selectedFamily -- Decrease maxPeople
+				
+					DELETE FROM @familyList WHERE FamilyId = @selectedFamily -- Clean
 				END
+
+				--Clean Variables
+				SELECT @selectedFamily = NULL, @selectedUser = NULL
+
+				--Select again a new item
+				SELECT TOP(1) @selectedFamily = FamilyId, @selectedUser = UserId
+				FROM @familyList
+				WHERE MembersCount <= @maxPeople
+				ORDER BY newid()
+		END
+	
+		SELECT @numPeople = COUNT(0)
+		FROM @people2Kick
+
+		IF (@numPeople > 0)
+		BEGIN
+			EXEC P_Move2Queued 
+			@eventId = @eventId,
+			@insertedTime = @insertedTime,
+			@userList = @people2kick
+		END
+	END
 RETURN 0
 
