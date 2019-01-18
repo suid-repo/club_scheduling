69a6ba7e195f6c954aa44c4dd11c53aadf17e844
Christopher Lang <S00173479@mail.itsligo.ie>
2019-01-18 14:52:08 +0000

    Skeleton update, comments, needs more

--- WebApplication/Controllers/FamiliesController.cs
+++ WebApplication/Controllers/FamiliesController.cs
@@ -15,7 +15,8 @@ namespace WebApplication.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // Does this go here?? [Authorize(Roles = "Owner")]
+        // Does this go here?? 
+        [Authorize(Roles = "Owner")]
         // GET: Families
         public ActionResult Index()
         {
@@ -27,7 +28,10 @@ namespace WebApplication.Controllers
         }
 
         [Authorize(Roles = "Owner")]
+        // All family members can see their family details
+        //[Authorize(Roles = "FamilyMember")]
         // GET: Families/Details/5
+        // Instead of "my" could details be used instead??
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -42,14 +46,26 @@ namespace WebApplication.Controllers
             return View(family);
         }
 
-        // When you create a family you get the owner role for that family [Authorize(Roles = "Owner")]
+        // Get the current user
+        /*
+         * public ApplicationUser getUserDetails()
+        {
+            ApplicationDbContext AppAuthDb = new ApplicationDbContext();
+            return AppAuthDb.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
+        }
+        */
+
+        // When you create a family you get given the owner role for that family 
+        [Authorize(Roles = "Owner")]
         // GET: Families/Create
         public ActionResult Create()
         {
             return View();
+            //This will work or needs more??
+            getUserDetails(Roles = "Owner");
         }
-        // POST: Families/Create
 
+        // POST: Families/Create
         // When you create a family you get the owner role for that family [Authorize(Roles = "Owner")]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -64,17 +80,10 @@ namespace WebApplication.Controllers
 
             return View(family);
         }
-        // Get the current user
-        /*
-         * public ApplicationUser getUserDetails()
-        {
-            ApplicationDbContext AppAuthDb = new ApplicationDbContext();
-            return AppAuthDb.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
-        }
-        */
 
         [Authorize(Roles = "Owner")]
         // GET: Families/Edit/5
+        // This is where the family owner can edit their family members level etc.
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -88,8 +97,8 @@ namespace WebApplication.Controllers
             }
             return View(family);
         }
-        // POST: Families/Edit/5
 
+        // POST: Families/Edit/5
         [Authorize(Roles = "Owner")]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -105,6 +114,8 @@ namespace WebApplication.Controllers
         }
 
         [Authorize(Roles = "Owner")]
+        // The family owner can delete the family, existing accounts get removed from the family
+        // and unactivated kids accounts get deleted
         // GET: Families/Delete/5
         public ActionResult Delete(int? id)
         {
@@ -119,8 +130,8 @@ namespace WebApplication.Controllers
             }
             return View(family);
         }
-        // POST: Families/Delete/5
 
+        // POST: Families/Delete/5
         [Authorize(Roles = "Owner")]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
