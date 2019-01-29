87669ed1ab788954bdb80ddb3b4ddde65e528409
Jonathan <jpierre@suid.fr>
2019-01-29 11:13:07 +0000

    Level ApplicationUser Added

--- WebApplication/Models/IdentityModels.cs
+++ WebApplication/Models/IdentityModels.cs
@@ -15,6 +15,7 @@ namespace WebApplication.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string BirthDay { get; set; }
+        public virtual Level Level { get; set; }
         public virtual Family Family { get; set; }
         public virtual ICollection<Event> Events { get; set; }
         public virtual ICollection<CoachEvent> CoachEvents { get; set; }
