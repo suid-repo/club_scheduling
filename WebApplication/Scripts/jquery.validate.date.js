86274d50b1a18f3806208755c0ed6501a1bfd337
Jonathan PIERRE <jpierre@suid.fr>
2019-03-20 23:11:40 +0100

    MAJOR DATE FIXE

--- WebApplication/Scripts/jquery.validate.date.js
+++ WebApplication/Scripts/jquery.validate.date.js
@@ -1,11 +1,38 @@
 ﻿$(function () {
     $.validator.methods.date = function (value, element) {
-        if ($.browser.webkit) {
-            var d = new Date();
-            return this.optional(element) || !/Invalid|NaN/.test(new Date(d.toLocaleDateString(value)));
+        //if ($.browser.webkit) {
+        //    var d = new Date();
+        //    return this.optional(element) || !/Invalid|NaN/.test(new Date(d.toLocaleDateString(value)));
+        //}
+        //else {
+        //    return this.optional(element) || !/Invalid|NaN/.test(new Date(value));
+        //}
+
+        //CREATE NEW DATE
+        //BECAUSE JAVACRIPT DON'T KNOW dd/MM/yyy format, we have to do it manually...
+        var d = value.split('/');
+        var h = value.split(' ')[1];
+        var day = parseInt(d[0]);
+        var month = parseInt(d[1]);
+        var year = parseInt(d[2]);
+
+        if (h) {
+            h = h.split(':');
+            var hour = parseInt(h[0]);
+            var min = parseInt(h[1]);
+            var s = parseInt(h[3]);
+
+            (isNaN(s)) ? s = 0 : null;
+
+            d = new Date(year, month, day, hour, min, s);
         }
-        else {
-            return this.optional(element) || !/Invalid|NaN/.test(new Date(value));
+        else
+        {
+            d = new Date(year, month, day);
         }
+        
+        
+
+        return this.optional(element) || !/Invalid|NaN/.test(d);
     };
 });
